cb81fff328b718fe22162bbb35e764bc26896850
Dinma-Faith <phaytep@gmail.com>
2022-07-05 22:51:57 +0100

    Add data and queries to table

--- data.sql
+++ data.sql
@@ -3,3 +3,34 @@
 INSERT INTO animals (name) VALUES ('Luna');
 INSERT INTO animals (name) VALUES ('Daisy');
 INSERT INTO animals (name) VALUES ('Charlie');
+
+INSERT INTO animals (
+  name,
+  date_of_birth,
+  escape_attempts,
+  neutered,
+  weight_kg
+)
+VALUES (
+  'Agumon',
+  date '2020-02-03',
+  0,
+  'true',
+  10.23
+);
+
+INSERT INTO animals (
+  name,
+  date_of_birth,
+  escape_attempts,
+  neutered,
+  weight_kg)
+VALUES (
+  'Gabumon',
+  date '2018-11-15',
+  2,
+  'true',
+  8
+);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Pikachu', date '2021-1-7', 1, 'false', 15.04),('Devimon', date '2017-5-12', 5, 'true', 11);
--- schema.sql
+++ schema.sql
@@ -6,6 +6,7 @@ CREATE TABLE animals (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR (100),
     date_of_birth DATE,
+    escape_attempts INT,
     neutered BOOLEAN,
     weight_kg DECIMAL
 );
